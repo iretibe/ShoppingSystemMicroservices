4a403f677bfa51c78458b9914824a4cd8aadfdf2
Somad <iretibe@users.noreply.github.com>
2021-05-12 11:21:53 +0000

    Worked on Order Microservices with  Clean Architecture and CQRS implementation

--- Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
+++ Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -5,13 +5,17 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Polly;
 using System;
+using System.Threading;
 
 namespace Ordering.API.Extensions
 {
     public static class HostExtensions
     {
-        public static IHost MigrateDatabase<TContext>(this IHost host, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
+        public static IHost MigrateDatabase<TContext>(this IHost host, 
+            Action<TContext, IServiceProvider> seeder, int? retry = 0) where TContext : DbContext
         {
+            int retryForAvailaility = retry.Value;
+
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
@@ -22,32 +26,44 @@ namespace Ordering.API.Extensions
                 {
                     logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
 
-                    var retry = Policy.Handle<SqlException>()
-                            .WaitAndRetry(
-                                retryCount: 5,
-                                sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), // 2,4,8,16,32 sc
-                                onRetry: (exception, retryCount, context) =>
-                                {
-                                    logger.LogError($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
-                                });
-
-                    //if the sql server container is not created on run docker compose this
-                    //migration can't fail for network related exception. The retry options for DbContext only 
-                    //apply to transient exceptions                    
-                    retry.Execute(() => InvokeSeeder(seeder, context, services));
+                    InvokeSeeder(seeder, context, services);
 
                     logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
+
+                    //var retry = Policy.Handle<SqlException>()
+                    //        .WaitAndRetry(
+                    //            retryCount: 5,
+                    //            sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), // 2,4,8,16,32 sc
+                    //            onRetry: (exception, retryCount, context) =>
+                    //            {
+                    //                logger.LogError($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+                    //            });
+
+                    ////if the sql server container is not created on run docker compose this
+                    ////migration can't fail for network related exception. The retry options for DbContext only 
+                    ////apply to transient exceptions                    
+                    //retry.Execute(() => InvokeSeeder(seeder, context, services));
+
+                    //logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
                 }
                 catch (SqlException ex)
                 {
                     logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
+
+                    if (retryForAvailaility < 50)
+                    {
+                        retryForAvailaility++;
+                        Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, seeder, retryForAvailaility);
+                    }
                 }
             }
 
             return host;
         }
 
-        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context, IServiceProvider services)
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, 
+            TContext context, IServiceProvider services)
             where TContext : DbContext
         {
             context.Database.Migrate();
