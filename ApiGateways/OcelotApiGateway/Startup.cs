deaf84780e82e23c2618d7b784ecb45c5dd4aafc
Somad <iretibe@users.noreply.github.com>
2021-05-27 12:47:12 +0000

    Built API Gateway with Ocelot and applied Gateway Routing Pattern

--- ApiGateways/OcelotApiGateway/Startup.cs
+++ ApiGateways/OcelotApiGateway/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using System.Threading.Tasks;
@@ -15,11 +16,15 @@ namespace OcelotApiGateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot();
+            //services.AddOcelot();
+            services.AddOcelot().AddCacheManager(opt =>
+            {
+                opt.WithDictionaryHandle();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async Task ConfigureAsync(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
