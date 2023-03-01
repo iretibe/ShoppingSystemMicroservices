1e1529148fdd119def8d20d4e35e2e6240e71a96
iretibe <yessouf2009@yahoo.fr>
2023-03-01 14:24:01 +0000

    Updated packages

--- Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
+++ Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -25,7 +25,7 @@ namespace Ordering.API.EventBusConsumer
         public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
         {
             var command = _mapper.Map<CheckoutOrderCommand>(context.Message);
-            var result = _mediator.Send(command);
+            var result = await _mediator.Send(command);
 
             _logger.LogInformation("BasketCheckoutEvent consumed successfully. Created Order Id : {newOrderId}", result);
         }
